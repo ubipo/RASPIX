f04c2811258d138599b4e4e0e7f861ce29905964
Ubipo <Ubipo.Skippy@gmail.com>
2017-04-15 00:19:57 +0200

    Added external settings file

--- python/RASPIX.py
+++ python/RASPIX.py
@@ -1,15 +1,36 @@
 import Tkinter
+import time
+from time import sleep
 from tempfile import NamedTemporaryFile
 from Tkinter import *
 from PIL import Image, ImageTk
 from resizeimage import resizeimage
 from smb.SMBConnection import SMBConnection
 
+delay = 1000
+
+
+# Provide the SMB server info in a text document 'SMB_info.txt' witch structure:
+#<IP>
+#<Port>
+#<Server Name>
+#<Shared folder name>
+#<Path>
+#<Username>
+#<Password>
+
+f = open("smb_info.txt")
+smb_info = []
+for line in f:
+   smb_info.append(line.strip())
+print 'SMB Server configuration: ' + str(smb_info)
+f.close()
+
 class Fotokadron9000(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         self.init_GUI()
-        self.connect_to_DiskStation()
+        self.connect_to_smb()
         self.download_new_image()
         self.cycle()
 
@@ -28,23 +49,23 @@ class Fotokadron9000(Frame):
         self.pack(fill=BOTH, expand=1)
         self.i = 0
 
-    def connect_to_DiskStation(self):
-        self.conn = SMBConnection(username='Pieter',
-                                  password='Pieter',
+    def connect_to_smb(self):
+        self.conn = SMBConnection(username=smb_info[5],
+                                  password=smb_info[6],
                                   my_name='RASPIX',
-                                  remote_name='DiskStation',
+                                  remote_name=smb_info[2],
                                   use_ntlm_v2=True)
-        assert self.conn.connect(ip='192.168.1.15',
-                                 port=139)
+        assert self.conn.connect(ip=smb_info[0],
+                                 port=int(smb_info[1]))
 
     def download_new_image(self):
-        print('Downloading new images from DiskStation')
+        print('Downloading new images from SMB Server')
         self.list_images()
         image_file = NamedTemporaryFile()
         filename = self.image_list[self.i][0]
         print filename
-        self.conn.retrieveFile('Photo',
-                               'RASPIX/'+filename,
+        self.conn.retrieveFile(smb_info[3],
+                               smb_info[4]+filename,
                                 image_file)
         self.original = Image.open(image_file)
         self.original.load()
@@ -54,7 +75,7 @@ class Fotokadron9000(Frame):
     def list_images(self):
         print('Creating new image list')
         self.image_list = []
-        l = self.conn.listPath('Photo', 'RASPIX')
+        l = self.conn.listPath(smb_info[3], smb_info[4])
         def is_image(shared_file):
             filename = shared_file.filename.lower()
             return (filename[-5:] == '.jpeg'
@@ -67,8 +88,9 @@ class Fotokadron9000(Frame):
 
     def cycle(self):
         self.draw_image()
-        self.download_new_image() 
-        self.master.after(500, self.cycle)
+        self.download_new_image()
+        delta = int(delay-(time.time()*1000-self.time_before))
+        self.master.after(delta, self.cycle)
 
     def draw_image(self):
         print('Drawing new image')
@@ -76,6 +98,7 @@ class Fotokadron9000(Frame):
         resized = resizeimage.resize_contain(self.original, self.screen_dim)
         self.image = ImageTk.PhotoImage(resized)
         self.display.delete("IMG")
+        self.time_before = time.time() * 1000
         self.display.create_image(0, 0, image=self.image, anchor=NW, tags="IMG")
 
         
